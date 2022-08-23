bac210b7379d75c675a15b15a02f2d1cdcc4ed29
JacobDangCS50 <jacobohdang@gmail.com>
2022-08-22 21:03:12 -0700

    lab 6 attempt1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//CALLS AND SETS STATES & FORM BELOW
+
+class App extends React.Component() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: [],
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.getCityData}>
+          <label> Pick a city!
+            <input type='text' onInput={this.handleInput} />
+            <button type='submit'> Explore! </button>
+          </label>
+        </form>
+        {this.state.error ? <p>{this.state.error.message}</p> : <p>{this.state.cityData}</p>}
+      </>
+    );
+  }
 }
+
+
+//FUNCTIONS OUTSIDE OF RENDER BELOW
+
+
+handleInput = (e) => {
+  e.preventDefault();
+  this.setState({
+    city.e.target.value
+  })
+}
+
+getCityData = async (e) => {
+  e.preventDefault();
+  try {
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.city}&format.json`;
+
+    let cityData = await axios.get(url)
+  } catch (error) {
+    this.setState{
+      (
+        error: true,
+        errorMessage: error.message
+      )}
+  }
+}
+
 
 export default App;
